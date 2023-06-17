bf7822c1f8b791b3ef28206c897dcbc7a48cda6f
CyberCat03 <134807945+CyberCat03@users.noreply.github.com>
2023-06-16 23:05:44 -0600

    Clase administrador

---
+++ Administrador.java
@@ -0,0 +1,23 @@
+
+public class Administrador {
+    private String identificador;
+    private String nombre;
+    private String contrasena;
+
+    public Administrador(String identificador, String nombre) {
+        this.identificador = identificador;
+        this.contrasena = contrasena;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+}
